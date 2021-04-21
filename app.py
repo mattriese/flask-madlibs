a3f9932023169ce6b09f7b265de88c9ad5d6c2f8
Matt Riese <rise.matt@gmail.com>
2021-04-21 16:54:29 -0700

    working on loop in template

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def show_start_form():
+
+    parts_of_speech = silly_story.prompts
+
+    return render_template("questions.html", noun=parts_of_speech)
