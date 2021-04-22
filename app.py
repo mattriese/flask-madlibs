07a3a011c1325e98f5d120fb070f8dce9b251ec4
Perryvon <Perry.j.vr@gmail.com>
2021-04-21 17:57:16 -0700

    rough working draft of madlib game

--- app.py
+++ app.py
@@ -10,7 +10,17 @@ debug = DebugToolbarExtension(app)
 
 @app.route("/")
 def show_start_form():
-
+    ''' loads form with variable number of part_of_speech input forms ''' 
     parts_of_speech = silly_story.prompts
 
-    return render_template("questions.html", noun=parts_of_speech)
+    return render_template("questions.html", pos_list = parts_of_speech)
+
+@app.route("/story")
+def show_story():
+    ''' generates a madlib from the users inputs and displays text onto page ''' 
+        # we need {place:paris, noun:taco, verb:run...}
+
+    user_input_result = request.args
+    user_story = silly_story.generate(user_input_result)
+    
+    return render_template("story.html", user_generated_story = user_story)
